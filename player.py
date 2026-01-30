c8873f1f966dd34bbf590bf600fc6396278a91f2
thatredheadgirl <fran.fairclough4@gmail.com>
2026-01-30 21:43:35 +0000

    added player sprite and rotation

---
+++ player.py
@@ -0,0 +1,34 @@
+import pygame
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        color = "white"
+        points = self.triangle()
+        pygame.draw.polygon(screen, color, points, LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
