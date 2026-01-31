58d57ed9bb2e71d7340038382b53db07a6f897a7
thatredheadgirl <fran.fairclough4@gmail.com>
2026-01-31 14:15:14 +0000

    forward and backward movement

---
+++ asteroid.py
@@ -0,0 +1,6 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid:
+    def __init__(self, x, y, radius):
+        super().__init__()
--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -21,11 +26,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+    
         pygame.display.flip()
-        dt = clock.tick(60)
-        
+        dt = clock.tick(60) / 1000
+        updatable.update(dt) 
 
 
 
